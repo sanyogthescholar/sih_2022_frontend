f5ae8bedf9020b0a79e747675befc8555dba52c9
sanyogthescholar <sanyogjain5678@gmail.com>
2022-03-09 12:55:30 +0530

    Update service worker registration

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -26,13 +26,13 @@ type Config = {
 export function register(config?: Config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    /*const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
       return;
-    }*/
+    }
 
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
@@ -114,11 +114,11 @@ function checkValidServiceWorker(swUrl: string, config?: Config) {
         (contentType != null && contentType.indexOf('javascript') === -1)
       ) {
         // No service worker found. Probably a different app. Reload the page.
-        /*navigator.serviceWorker.ready.then((registration) => {
+        navigator.serviceWorker.ready.then((registration) => {
           registration.unregister().then(() => {
             window.location.reload();
           });
-        });*/
+        });
       } else {
         // Service worker found. Proceed as normal.
         registerValidSW(swUrl, config);
