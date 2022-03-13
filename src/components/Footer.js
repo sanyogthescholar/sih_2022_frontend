00bba1ac07bea3ea61556f5ae44e8b447f68b6d7
jay-shinde <jayesh.shinde@vit.edu.in>
2022-03-13 19:35:09 +0530

    footer fixed 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
   
   return (
    
-    <footer className='bg-dark text-light py-3 mt-3 mb-max' id="footer">
+    <footer className='bg-dark text-light py-3 mt-3' id="footer">
       <p className='text-center'> Copyright &copy; Shadanan</p>
  
     </footer>
