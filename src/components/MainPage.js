294280bf07c8fa302e09260b450496be057ab189
mrunaltamhane <mrunal.tamhane7@gmail.com>
2022-03-13 13:46:58 +0530

    fixed nav toggle bar issue

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,53 +4,52 @@ class MainPage extends React.Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark   navbar-default " id="navbar">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              <img
-                src={require("../tickito_dark_logo.png")}
-                alt="logo"
-                width="50px"
-                height="50px"
-              />
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse navbar-default"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a
-                    className="nav-link active"
-                    id="addticket"
-                    aria-current="page"
-                    href="#"
-                  >
-                    Add Ticket
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="nav-link font-weight-bold"
-                    id="dashboard"
-                    href="#"
-                  >
-                    Dashboard
-                  </a>
-                </li>
-              </ul>
-            </div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a class="navbar-brand" href="#">
+            {" "}
+            <img
+              src={require("../t.png")}
+              width="50"
+              height="50"
+              alt="logo"
+              id="logo"
+            />
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item ">
+                <a
+                  className="nav-link active"
+                  id="addticket"
+                  aria-current="page"
+                  href="#"
+                >
+                  Add Ticket
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  className="nav-link font-weight-bold"
+                  id="dashboard"
+                  href="#"
+                >
+                  Dashboard
+                </a>
+              </li>
+            </ul>
+          </div>
           </div>
         </nav>
       </div>
